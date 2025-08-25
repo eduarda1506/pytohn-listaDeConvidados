127d35deb3ad9911bf23706a5cf162dc6f49d47f
eduarda1506 <eduarda.aparecida.pinto@escola.pr.gov.br>
2025-08-25 10:49:52 -0300

    criei as listas

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-convidados_da_festa = ['Ana','Diego']
+convidados_da_festa = ['Ana', 'Diego', 'Yuliany']
+
+status_presenca = {}
+
+# lista de pessoas que tentaram entrar
+lista_de_chegadas = ['Ana', 'Diego']
+
